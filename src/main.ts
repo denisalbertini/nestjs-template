cfa39259bd44361ce2d6e35e8582e8e70842fc93
Denis Albertini <denis.s.albertini@gmail.com>
2025-12-21 22:30:40 -0300

    feat fastify http adapter

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { configureApp } from './config/app.config';
 
 (async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   configureApp(app);
 
   const configService = app.get(ConfigService);
 
-  await app.listen(configService.get('PORT') || 3000);
+  await app.listen(configService.get<string>('PORT') || 3000);
 })();
